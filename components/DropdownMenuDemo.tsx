8eb06308b025da57d344c484fea424e47c4998e6
Muhammad Kamran <thaheemop@gmail.com>
2025-02-07 13:51:11 +0500

    Update DropdownMenuDemo.tsx

--- components/DropdownMenuDemo.tsx
+++ components/DropdownMenuDemo.tsx
@@ -14,6 +14,7 @@ import {
   User,
   UserPlus,
   Users,
+  Linkedin,
 } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
@@ -128,6 +129,12 @@ export function DropdownMenuDemo() {
           <DropdownMenuItem>
             <Github />
             <span>GitHub</span>
+          </DropdownMenuItem>
+        </Link>
+        <Link href="https://www.linkedin.com/in/muhammad-kamran-916b752b4/">
+          <DropdownMenuItem>
+            <Linkedin />
+            <span>LinkedIn</span>
           </DropdownMenuItem>
         </Link>
         <Link href="https://web.facebook.com/profile.php?id=100080963443012">
